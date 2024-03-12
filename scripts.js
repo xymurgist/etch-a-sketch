9e1e518270cad739a58397024343075cb9425862
“xymurgist” <pps.jim@gmail.com>
2024-03-12 16:48:54 -0600

    Divide processes. Filter user input. Add input labels.
    Divided actions into separate functions. Filter user input for rows and columns and adjust accordingly. Added labels to inputs

--- scripts.js
+++ scripts.js
@@ -18,29 +18,44 @@ function removeGrid() {
     }
 }
 
-function createGrid() {
 
-    // Clear the current grid before creating a new grid
-    removeGrid();
-
+function checkUserInput() {
     let gridRows = document.querySelector('#gridRows').value;
     let gridCols = document.querySelector('#gridCols').value;
-    let squaredGrid = gridSize * gridSize;
-    let count = 1;
 
-    if (isNaN(gridRows) || gridRows < 1 || isNaN(gridCols) || gridCols < 1) {
-        gridRows = 10;
-        gridCols = 10;
-        squaredGrid = 100;
+    if (gridRows > 100) {
+        gridRows = 100;
     }
 
-    for (let row = 0; row < gridRows; row++) {
+    if (gridCols > 100) {
+        gridCols = 100;
+    }
+
+    if (isNaN(gridRows) || gridRows < 1) {
+        gridRows = 1;
+    }
+
+    if (isNaN(gridCols) || gridCols < 1) {
+        gridCols = 1;
+    }
+
+    return [gridRows, gridCols];
+}
+
+
+function createGrid(userInput) {
+
+    let userRows = userInput[0]
+    let userCols = userInput[1]
+    let count = 1;
+
+    for (let row = 0; row < userRows; row++) {
         
         const gridRow = document.createElement('div');
         gridRow.classList.add("flex-container", "gridRow");
         container.appendChild(gridRow);
 
-        for (let column = 0; column < gridCols; column++) {
+        for (let column = 0; column < userCols; column++) {
             const gridCol = document.createElement('div');
             gridCol.classList.add("gridBox");
             // gridBox.style.flex = "1 0 1%";
@@ -52,6 +67,12 @@ function createGrid() {
     
 }
 
-submitButton.addEventListener("click", createGrid);
+function gridManager() {
+    removeGrid();
+    let userInput = checkUserInput();
+    createGrid(userInput);
+}
+
+submitButton.addEventListener("click", gridManager);
 
 // createFlexBoxes();
