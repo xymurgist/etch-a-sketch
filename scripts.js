5eff998fd5ebc7d83699099c4f92cb23476f3540
“xymurgist” <pps.jim@gmail.com>
2024-03-13 13:14:42 -0600

    Add listener for 'Enter' key.
    Added listener to row and column inputs for 'Enter' key to create grid.
    Added more TODOs.

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@
 
 const container = document.querySelector('#container');
 const submitButton = document.querySelector('#submitButton');
+const enterRows = document.querySelector('#userRows');
+const enterCols = document.querySelector('#userCols');
 
 
 // TODO //
-// Create logic to accept user input to create number of boxes for "grid" of flexbox <div>s
-// // prompt for user input for rows and columns for grid; start with hard numbers for testing
-// // use loop to create individual <div>s
-
 // Toggle highlight state of individual <div>s
+/// lookup mouseDown and mouseEnter
+/// make reset button
 
 function removeGrid() {
 
@@ -20,52 +20,60 @@ function removeGrid() {
 
 
 function checkUserInput() {
-    let gridRows = document.querySelector('#gridRows').value;
-    let gridCols = document.querySelector('#gridCols').value;
+    let userRows = document.querySelector('#userRows').value;
+    let userCols = document.querySelector('#userCols').value;
 
-    if (gridRows > 100) {
-        gridRows = 100;
+    if (userRows > 100) {
+        userRows = 100;
     }
 
-    if (gridCols > 100) {
-        gridCols = 100;
+    if (userCols > 100) {
+        userCols = 100;
     }
 
-    if (isNaN(gridRows) || gridRows < 1) {
-        gridRows = 1;
+    if (isNaN(userRows) || userRows < 1) {
+        userRows = 5;
     }
 
-    if (isNaN(gridCols) || gridCols < 1) {
-        gridCols = 1;
+    if (isNaN(userCols) || userCols < 1) {
+        userCols = 10;
     }
 
-    return [gridRows, gridCols];
+    return [userRows, userCols];
 }
 
 
 function createGrid(userInput) {
 
-    let userRows = userInput[0]
-    let userCols = userInput[1]
-    let count = 1;
+    let gridRows = userInput[0]
+    let gridCols = userInput[1]
 
-    for (let row = 0; row < userRows; row++) {
+    // create a row and fill with columns
+    for (let row = 0; row < gridRows; row++) {
         
         const gridRow = document.createElement('div');
-        gridRow.classList.add("flex-container", "gridRow");
+        gridRow.classList.add('flex-container');
         container.appendChild(gridRow);
 
-        for (let column = 0; column < userCols; column++) {
+        for (let column = 0; column < gridCols; column++) {
             const gridCol = document.createElement('div');
-            gridCol.classList.add("gridBox");
-            // gridBox.style.flex = "1 0 1%";
-            gridCol.textContent = count;
+            gridCol.classList.add('gridBox');
             gridRow.appendChild(gridCol);
-            count += 1;
         }
+
+        
+    }
+
+    // make box height = box width
+    let boxWidth = document.querySelector('.gridBox').clientWidth;
+    let boxes = document.querySelectorAll('.gridBox');
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].setAttribute("style", `height: ${boxWidth}px`);
     }
     
 }
+
 
 function gridManager() {
     removeGrid();
@@ -73,6 +81,27 @@ function gridManager() {
     createGrid(userInput);
 }
 
+
 submitButton.addEventListener("click", gridManager);
+
+// Would like to learn how to make the following more consice and reusable
+// If the user presses the "Enter" key on the keyboard
+enterRows.addEventListener("keypress", function (event){
+    if(event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        gridManager();
+    }
+});
+
+enterCols.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        gridManager();
+    }
+});
 
 // createFlexBoxes();
